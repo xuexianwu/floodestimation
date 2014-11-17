eb0c5736ab408689ebcd9fea6fb15f273e2985cc
faph <faph@users.noreply.github.com>
2014-11-17 14:22:40 +0000

    use distribution's `rvs` method for random sample

--- floodestimation/simulation.py
+++ floodestimation/simulation.py
@@ -58,7 +58,7 @@ class H2Simulation(object):
         # Generated records using pooling group kappa distribution for all donors at once
         kap_para = lm.pelkap(self.lmom_p)
         kappa_distr = distr.kap(loc=kap_para[0], scale=kap_para[1], k=kap_para[2], h=kap_para[3])
-        record_sim_all = kappa_distr.ppf(np.random.random(n_p * n_sim))
+        record_sim_all = kappa_distr.rvs(size=n_p * n_sim)
 
         record_start = 0
         # Simulated test statistic: variability V2
